9c3cfdddd5dc050ff43927f8bea6fbf0aa4cbf54
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-01-02 10:42:25 +0000

    Bind events to selector

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,7 +3,7 @@ export class UserForm {
 
   eventsMaps(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick()
+      'click:button': () => console.log('CLICKED')
     };
   }
 
@@ -12,13 +12,27 @@ export class UserForm {
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click me!</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMaps();
+
+    for (let key in eventsMap) {
+      const [event, selector] = key.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element: Element) => {
+        element.addEventListener(event, eventsMap[key]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
