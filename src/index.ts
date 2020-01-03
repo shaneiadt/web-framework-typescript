d2a79419ac18a496ff993b9ae953bbf422890d37
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2020-01-03 10:24:23 +0000

    Add region mapping to DOM elements for show & edit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { User } from './models/User';
 import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 
 (async () => {
   const user = await User.buildUser({ id: 4 }, 'http://localhost:3000/users');
   const root = document.getElementById('root');
 
   if (root) {
-    const userForm = new UserForm(root, user);
-    userForm.render();
+    const userEdit = new UserEdit(root, user);
+    userEdit.render();
+
+    console.log({ userEdit });
   }
 
   // const collection = await User.buildCollection('http://localhost:3000/users');
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,12 +1,21 @@
 import { Model } from '../models/Model';
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
   abstract template(): string;
-  abstract eventsMap(): { [key: string]: () => void };
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindModel(): void {
     this.model.on('change', () => {
@@ -29,11 +38,25 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
   render = (): void => {
     this.parent.innerHTML = '';
     const templateElement: HTMLTemplateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
